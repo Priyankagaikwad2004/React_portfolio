b7327d1e049d0dac0081be615c44a39413aa8b23
priyankagaikwad <priya937027@gmail.com>
2025-08-30 16:01:38 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Hero from './components/Hero';
 import About from './components/About';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
-import Contact from './components/contact';
+import Contact from './components/Contact';
 import Footer from './components/footer';
 import FloatingParticles from './components/FloatingParticles';
 
